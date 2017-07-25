ed5c95f520e2cc3450110836ffe091cbe7be3d5e
tongxinkai <863447329@qq.com>
2017-07-25 17:52:35 +0800

    增加了图片加载和权限管理和日志管理类

---
+++ lib_framework/src/main/java/com/ane/lib_uiframework/utils/log/XPrinter.java
@@ -0,0 +1,41 @@
+package com.ane.lib_uiframework.utils.log;
+
+import android.os.Build;
+
+public class XPrinter {
+    static final int MAX_LENGTH_OF_SINGLE_MESSAGE = 3500;
+
+    public static String lineSeparator = lineSeparator();
+
+    public static void println(int logLevel, String tag, String msg) {
+        if (msg.length() <= MAX_LENGTH_OF_SINGLE_MESSAGE) {
+            printChunk(logLevel, tag, msg);
+            return;
+        }
+
+        int msgLength = msg.length();
+        int start = 0;
+        int end = start + MAX_LENGTH_OF_SINGLE_MESSAGE;
+        while (start < msgLength) {
+            printChunk(logLevel, tag, msg.substring(start, end));
+
+            start = end;
+            end = Math.min(start + MAX_LENGTH_OF_SINGLE_MESSAGE, msgLength);
+        }
+    }
+
+    private static void printChunk(int logLevel, String tag, String msg) {
+        android.util.Log.println(logLevel, tag, msg);
+    }
+
+    private static String lineSeparator() {
+        try {
+            Class.forName("android.os.Build");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                return System.lineSeparator();
+            }
+        } catch (Exception ignored) {
+        }
+        return "\n";
+    }
+}
